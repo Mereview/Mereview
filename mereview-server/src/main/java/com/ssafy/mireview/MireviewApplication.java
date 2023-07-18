480b063c561e2f22e7ba13b2f5fcbb834213417e
유승민 <byuri1356@gmail.com>
2023-07-18 14:24:00 +0900

    Initial commit

---
+++ mereview-server/src/main/java/com/ssafy/mireview/MireviewApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mireview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MireviewApplication.class, args);
+	}
+
+}
---
+++ mereview-server/src/test/java/com/ssafy/mireview/MireviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.mireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
