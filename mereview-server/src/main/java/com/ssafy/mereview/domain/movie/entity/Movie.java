7de8bde7693f3fbb95af0f49b6e8cd49786b5779
유승민 <byuri1356@gmail.com>
2023-07-20 18:01:12 +0900

    S09P12C211-120/FEAT-MOVIE/Feat: 영화 Entity 개발중

--- mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/Movie.java
+++ mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/Movie.java
@@ -1,4 +1,36 @@
 package com.ssafy.mereview.domain.movie.entity;
 
-public class Movie {
+import com.ssafy.mereview.common.domain.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Movie extends BaseEntity {
+
+    @Id
+    @Column(name = "movie_id")
+    Long id;
+
+    @Column(name = "movie_title")
+    String title;
+
+    @Column(name = "movie_overview")
+    String overview;
+
+    @Column(name = "movie_populity")
+    Double populity;
+
+    @Column(name = "movie_poster_img")
+    String posterImg;
+
+    @Column(name = "movie_release_date")
+    String releaseDate;
+
+
 }
