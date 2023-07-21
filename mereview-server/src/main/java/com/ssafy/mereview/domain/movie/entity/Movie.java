7fc31de5541ab9855fa3bae73f6fc1d09932713f
유승민 <byuri1356@gmail.com>
2023-07-21 12:12:41 +0900

    S09P12C211-120/FEAT-MOVIE/feat: 영화 Entity 개발

--- mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/Movie.java
+++ mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/Movie.java
@@ -1,32 +1,51 @@
 package com.ssafy.mereview.domain.movie.entity;
 
 import com.ssafy.mereview.common.domain.BaseEntity;
+import com.ssafy.mereview.domain.review.entity.Review;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Movie extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "movie_id")
-    Long id;
+    private Long id;
 
-    String title;
+    private String title;
 
-    String overview;
+    private String overview;
 
-    Double populity;
+    private Double popularity;
 
-    String posterImg;
+    private String posterImg;
 
-    String releaseDate;
+    private String releaseDate;
 
+    @OneToMany(mappedBy = "movie")
+    private List<Review> reviews = new ArrayList<>();
 
+    @OneToMany(mappedBy = "movie")
+    private List<MovieGenre> movieGenres = new ArrayList<>();
 
+    @Builder
+    public Movie(Long id, String title, String overview, Double popularity, String posterImg, String releaseDate, List<Review> reviews, List<MovieGenre> movieGenres) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.posterImg = posterImg;
+        this.releaseDate = releaseDate;
+        this.reviews = reviews;
+        this.movieGenres = movieGenres;
+    }
 }
--- mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/MovieGenre.java
+++ mereview-server/src/main/java/com/ssafy/mereview/domain/movie/entity/MovieGenre.java
@@ -1,19 +1,35 @@
 package com.ssafy.mereview.domain.movie.entity;
 
 import com.ssafy.mereview.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MovieGenre extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @Column(name = "movie_genre_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @Builder
+    public MovieGenre(Long id, Movie movie, Genre genre) {
+        this.id = id;
+        this.movie = movie;
+        this.genre = genre;
+    }
 }
