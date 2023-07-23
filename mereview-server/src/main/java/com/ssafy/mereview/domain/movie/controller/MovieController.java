6192393f7ff88138e1922f63e91da9fadcfd17f1
byuri1356 <byuri1356@gmail.com>
2023-07-23 23:18:36 +0900

    S09P12C211-122/FEAT-MOVIE/FEAT: 영화 Domain 개발

--- mereview-server/src/main/java/com/ssafy/mereview/domain/movie/controller/MovieController.java
+++ mereview-server/src/main/java/com/ssafy/mereview/domain/movie/controller/MovieController.java
@@ -1,11 +1,19 @@
 package com.ssafy.mereview.domain.movie.controller;
 
+import com.ssafy.mereview.domain.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/movie")
+@RequiredArgsConstructor
 public class MovieController {
-    private final
+    private final MovieService movieService;
+
+    @
+
 
 }
