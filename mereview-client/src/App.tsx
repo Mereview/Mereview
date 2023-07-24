f6984bf1f38ff05c0d2676467df01fb91cc2f000
양진형 <jinhyugn@gmail.com>
2023-07-24 13:48:57 +0900

    S09P12C211-184/dev/init: 테스트 페이지 생성

--- mereview-client/src/App.tsx
+++ mereview-client/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
+import TestPage from "./pages/TestPage";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" Component={IndexPage}></Route>
+          <Route path="/test" Component={TestPage}></Route>
         </Routes>
       </Router>
     </div>
