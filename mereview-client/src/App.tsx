2bffa05ca9c94b00ae48879c440975f84c402525
ungseo <ungseo@gmail.com>
2023-07-24 11:57:01 +0900

    S09P12C211-136/dev/init: 라우터추가

--- mereview-client/src/App.tsx
+++ mereview-client/src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" Component={IndexPage}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
