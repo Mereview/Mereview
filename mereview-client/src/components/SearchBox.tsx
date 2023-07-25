0f3be5421e36c869af4e2f21f9d2833a8048971a
양진형 <jinhyugn@gmail.com>
2023-07-25 13:31:12 +0900

    S09P12C211-196/FEAT-COMPONENT/edit: 리액트 부트스트랩 모듈 추가

--- mereview-client/src/components/SearchBox.tsx
+++ mereview-client/src/components/SearchBox.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "../styles/css/SearchBox";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../styles/css/SearchBox.css";
 
 const SearchBox = () => {
-  return <div></div>;
+  return (
+    <div>
+      Hello?
+      <Row>
+        <Col>col1 ^^!</Col>
+        <div className="col">col2 ^&^</div>
+      </Row>
+      <div className="row">
+        <Col>col1 ^@^!</Col>
+        <div className="col">col2 ^&!^</div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBox;
