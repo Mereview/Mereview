a2f15bafc3e52fb627f4ca43e7262c30f455c221
mji4209 <mji4209@naver.com>
2023-08-02 16:15:12 +0900

    S09P12C211-263/PAGE-REVIEW-WRITE/feat:키워드 가중치 설정 개발

---
+++ mereview-client/src/components/common/KeywordSlider.tsx
@@ -0,0 +1,53 @@
+import Slider from "@mui/material/Slider";
+import React from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
+import Input from "./Input";
+import { Row, Col } from "react-bootstrap";
+import { Connect } from "react-redux";
+
+const KeywordSlider = forwardRef((props, ref) => {
+  const [keyInfo, setKeyInfo] = useState({ keyword: "", gauge: 0 });
+  const getKeyInfo = () => {
+    return keyInfo;
+  };
+  const infoHandler = (e) => {
+    if (typeof e.target.value === "number") {
+      const newKeyword = keyInfo.keyword;
+      const newGauge = e.target.value;
+      setKeyInfo({ keyword: newKeyword, gauge: newGauge });
+    } else {
+      const newKeyword = e.target.value;
+      const newGauge = keyInfo.gauge;
+      setKeyInfo({
+        keyword: newKeyword,
+        gauge: newGauge,
+      });
+    }
+  };
+  useImperativeHandle(ref, () => ({
+    getKeyInfo,
+  }));
+  return (
+    <div>
+      <Row className="mx-1 my-4">
+        <Col md={4}>
+          <input
+            className="form-control"
+            onChange={infoHandler}
+            value={keyInfo.keyword}
+          ></input>
+        </Col>
+        <Col md={2}>
+          <p>{keyInfo.gauge}</p>
+        </Col>
+        <Slider
+          value={keyInfo.gauge}
+          onChange={infoHandler}
+          style={{ width: "50%" }}
+        />
+      </Row>
+    </div>
+  );
+});
+
+export default KeywordSlider;
--- mereview-client/src/pages/ReviewWrite.tsx
+++ mereview-client/src/pages/ReviewWrite.tsx
@@ -3,9 +3,10 @@ import { useDropzone } from "react-dropzone";
 import { Button } from "../components/common";
 import React, { useState, useRef, useCallback } from "react";
 import "../styles/css/ReviewWrite.css";
-import Slide from "../components/common/Slide";
+import { Slider } from "@mui/material";
+import KeywordSlider from "../components/common/KeywordSlider";
 import { Input } from "../components/common";
-
+import { Provider } from "react-redux";
 const ReviewWrite = () => {
   const [nickname, setNickname] = useState("닉네임");
   const [profile, setProfile] = useState("/logo2.png");
@@ -14,6 +15,34 @@ const ReviewWrite = () => {
   const [reviewName, setReviewName] = useState<string | null>(null);
   const [movieName, setMovieName] = useState<string | null>(null);
   const [oneSentance, setOneSentance] = useState<string | null>(null);
+
+  const childRef1 = useRef(null);
+  const childRef2 = useRef(null);
+  const childRef3 = useRef(null);
+  const childRef4 = useRef(null);
+  const childRef5 = useRef(null);
+  const handleBtnClick = () => {
+    if (childRef1.current) {
+      const valueFromChild1 = childRef1.current.getKeyInfo();
+      console.log("Value from Child: ", valueFromChild1);
+    }
+    if (childRef2.current) {
+      const valueFromChild2 = childRef2.current.getKeyInfo();
+      console.log("Value from Child: ", valueFromChild2);
+    }
+    if (childRef3.current) {
+      const valueFromChild3 = childRef3.current.getKeyInfo();
+      console.log("Value from Child: ", valueFromChild3);
+    }
+    if (childRef4.current) {
+      const valueFromChild4 = childRef4.current.getKeyInfo();
+      console.log("Value from Child: ", valueFromChild4);
+    }
+    if (childRef5.current) {
+      const valueFromChild5 = childRef5.current.getKeyInfo();
+      console.log("Value from Child: ", valueFromChild5);
+    }
+  };
   const onDrop = useCallback((acceptedFiles: File[]) => {
     const file = acceptedFiles[0];
     if (file) {
@@ -46,15 +75,8 @@ const ReviewWrite = () => {
         margin: "auto",
       }}
     >
-      <Row className="mt-3 mx-4">
+      <Row className="mt-5 mx-4">
         <Col lg={8}>
-          {/* <Input
-            id="reviewName"
-            placeholder="리뷰 제목을 입력하세요"
-            styles="input-box"
-            onChange={reviewNameHandler}
-            type="text"
-          ></Input> */}
           <Form.Control
             placeholder="리뷰 제목을 입력하세요"
             className="border rounded-2 text-lg"
@@ -76,13 +98,6 @@ const ReviewWrite = () => {
       </Row>
       <Row className="mx-4 align-items-center">
         <Col md={6}>
-          {/* <Input
-            id="reviewName"
-            placeholder="영화 제목을 입력하세요"
-            styles="input-box"
-            onChange={reviewNameHandler}
-            type="text"
-          ></Input> */}
           <Form.Control
             placeholder="영화 제목을 입력하세요"
             className="border rounded-2 text-lg"
@@ -92,13 +107,6 @@ const ReviewWrite = () => {
       </Row>
       <Row className="mx-4 my-4 align-items-center">
         <Col sm={6}>
-          {/* <Input
-            id="reviewName"
-            placeholder="한줄평을 입력하세요"
-            styles="input-box"
-            onChange={reviewNameHandler}
-            type="text"
-          ></Input> */}
           <Form.Control
             placeholder="한줄평을 입력하세요"
             className="border rounded-2 text-lg"
@@ -115,33 +123,41 @@ const ReviewWrite = () => {
         <Col sm={1}>
           <div {...getRootProps()}>
             <input {...getInputProps()} />
-            <Button styles="btn-primary" text="첨부"></Button>
+            <Button styles="btn-fourth" text="첨부"></Button>
           </div>
         </Col>
         <Col />
       </Row>
       <Row className="mx-4">
         <Col md={6}>
-          <textarea style={{ resize: "none" }}></textarea>
-          {/* <textarea
+          <textarea
             className="border rounded-2 border-5 i-box form-control"
             style={{ resize: "none" }}
-          ></textarea> */}
+          ></textarea>
         </Col>
-        {/* <Col md={2} /> */}
-        {/* <Col
-          className="my-auto text-center border border-5 rounded-2"
+        <Col md={2} />
+        <Col
+          className="my-auto text-center border border-5 rounded-2 i-box"
           style={{ backgroundColor: "white" }}
         >
-          <p>키워드</p>
-          <Slide />
-          <Slide />
-          <Slide />
-          <Slide />
-          <Slide />
-        </Col> */}
+          <KeywordSlider ref={childRef1}></KeywordSlider>
+          <KeywordSlider ref={childRef2}></KeywordSlider>
+          <KeywordSlider ref={childRef3}></KeywordSlider>
+          <KeywordSlider ref={childRef4}></KeywordSlider>
+          <KeywordSlider ref={childRef5}></KeywordSlider>
+        </Col>
       </Row>
-      <Row lg={12}>test</Row>
+      <Row lg={12} className="mt-3 align-items-center">
+        <Col lg={9} />
+        <Col lg={1}>버튼</Col>
+        <Col className="t-right me-4">
+          <Button
+            styles="btn-primary"
+            onClick={handleBtnClick}
+            text="확인"
+          ></Button>
+        </Col>
+      </Row>
     </Container>
   );
 };
