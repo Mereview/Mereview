9535053adcc51622a8272afe63eb36886b91ed8b
이정현 <orcteacher@naver.com>
2023-08-04 14:47:17 +0900

    S09P12C211-324/FEAT-MEMBER/feat: 영화 조회 기능 추가

--- mereview-server/src/main/java/com/ssafy/mereview/api/controller/movie/MovieController.java
+++ mereview-server/src/main/java/com/ssafy/mereview/api/controller/movie/MovieController.java
@@ -1,9 +1,26 @@
 package com.ssafy.mereview.api.controller.movie;
 
+import com.ssafy.mereview.api.service.movie.MovieService;
+import com.ssafy.mereview.api.service.movie.dto.response.MovieResponse;
+import com.ssafy.mereview.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/movies")
+@Slf4j
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping("/{keyword}")
+    public ApiResponse<List<MovieResponse>> searchMovies(@PathVariable String keyword) {
+        log.debug("keyword = {}", keyword);
+        return ApiResponse.ok(movieService.searchMovies(keyword));
+    }
+
 }
--- mereview-server/src/main/java/com/ssafy/mereview/api/service/movie/dto/response/MovieResponse.java
+++ mereview-server/src/main/java/com/ssafy/mereview/api/service/movie/dto/response/MovieResponse.java
@@ -1,4 +1,57 @@
 package com.ssafy.mereview.api.service.movie.dto.response;
 
+import com.ssafy.mereview.domain.movie.entity.Movie;
+import com.ssafy.mereview.domain.movie.entity.MovieGenre;
+import com.ssafy.mereview.domain.review.entity.Review;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@NoArgsConstructor
 public class MovieResponse {
+
+
+    private Long id;
+
+    private int movieContentId;
+
+    private String title;
+
+    private String overview;
+
+    private Double voteAverage;
+
+    private String posterImg;
+
+    private String releaseDate;
+
+    @Builder
+    public MovieResponse(Long id, int movieContentId, String title, String overview, Double voteAverage, String posterImg, String releaseDate) {
+        this.id = id;
+        this.movieContentId = movieContentId;
+        this.title = title;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.posterImg = posterImg;
+        this.releaseDate = releaseDate;
+    }
+
+    public static MovieResponse of(Movie movie){
+        return MovieResponse.builder()
+                .id(movie.getId())
+                .movieContentId(movie.getMovieContentId())
+                .title(movie.getTitle())
+                .overview(movie.getOverview())
+                .voteAverage(movie.getVoteAverage())
+                .posterImg(movie.getPosterImg())
+                .releaseDate(movie.getReleaseDate())
+                .build();
+    }
 }
