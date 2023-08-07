13c26da450a9c1a58e1c7ec38d57e5c7e9654e17
최영환 <longbright0804@gmail.com>
2023-08-07 16:20:58 +0900

    S09P12C211-400/FEAT-REVIEW/feat: 관심사 리뷰 작성 알림

---
+++ mereview-server/src/main/java/com/ssafy/mereview/api/controller/review/NotificationController.java
@@ -0,0 +1,33 @@
+package com.ssafy.mereview.api.controller.review;
+
+import com.ssafy.mereview.api.service.review.NotificationService;
+import com.ssafy.mereview.api.service.review.dto.response.NotificationResponse;
+import com.ssafy.mereview.common.response.ApiResponse;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews/notifications")
+@RestController
+@Api(tags = {"리뷰 작성 알림 API"})
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PutMapping("/{notificationId}")
+    public ApiResponse<NotificationResponse> toggleStatus(@PathVariable Long notificationId) {
+        NotificationResponse response = notificationService.toggleStatus(notificationId);
+
+        return ApiResponse.ok(response);
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public ApiResponse<Long> deleteNotification(@PathVariable Long notificationId) {
+        Long deleteId = notificationService.delete(notificationId);
+
+        return ApiResponse.ok(deleteId);
+    }
+}
