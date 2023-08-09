adb21767947702d14ed5be55292536e7f47003c9
유승민 <byuri1356@gmail.com>
2023-08-10 03:40:30 +0900

    S09P12C211-340/dev/test: tsx jenkins auto publish test

--- mereview-client/src/App.tsx
+++ mereview-client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
-import ReviewHome from "./pages/ReviewHome";
+// import ReviewHome from "./pages/ReviewHome";
 import ReviewWrite from "./pages/ReviewWrite";
 import ReviewDetailPage from "./pages/ReviewDetailPage";
 import ProfilePage from "./pages/ProfilePage";
@@ -37,7 +37,7 @@ function App() {
         {isAuthenticated && user && <NavigationBar user={user} />}
         <Routes>
           <Route path="/" Component={IndexPage}></Route>
-          <Route path="/review" Component={ReviewHome}></Route>
+          {/* <Route path="/review" Component={ReviewHome}></Route> */}
           <Route path="/review/write" Component={ReviewWrite}></Route>
           <Route path="/review/:id" Component={ReviewDetailPage}></Route>
           <Route path="/profile" Component={ProfilePage}></Route>
