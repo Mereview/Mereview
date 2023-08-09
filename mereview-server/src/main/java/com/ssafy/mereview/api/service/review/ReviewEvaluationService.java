f8ae1a3df935c9d8ec5fb42927fdccefc111ffe5
최영환 <longbright0804@gmail.com>
2023-08-09 14:49:48 +0900

    S09P12C211-404/FEAT-REVIEW/refactor: 리팩토링 & 테스트

--- mereview-server/src/main/java/com/ssafy/mereview/api/service/review/ReviewEvaluationService.java
+++ mereview-server/src/main/java/com/ssafy/mereview/api/service/review/ReviewEvaluationService.java
@@ -2,6 +2,11 @@ package com.ssafy.mereview.api.service.review;
 
 import com.ssafy.mereview.api.service.review.dto.request.ReviewEvaluationServiceRequest;
 import com.ssafy.mereview.api.service.review.dto.response.ReviewEvaluationResponse;
+import com.ssafy.mereview.domain.member.entity.Member;
+import com.ssafy.mereview.domain.member.entity.MemberTier;
+import com.ssafy.mereview.domain.member.repository.MemberRepository;
+import com.ssafy.mereview.domain.member.repository.MemberTierQueryRepository;
+import com.ssafy.mereview.domain.member.repository.MemberTierRepository;
 import com.ssafy.mereview.domain.review.entity.ReviewEvaluation;
 import com.ssafy.mereview.domain.review.entity.ReviewEvaluationType;
 import com.ssafy.mereview.domain.review.repository.query.ReviewEvaluationQueryRepository;
@@ -11,7 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static com.ssafy.mereview.domain.review.entity.ReviewEvaluationType.*;
@@ -24,6 +31,7 @@ public class ReviewEvaluationService {
 
     private final ReviewEvaluationRepository evaluationRepository;
     private final ReviewEvaluationQueryRepository evaluationQueryRepository;
+    private final MemberTierQueryRepository memberTierQueryRepository;
 
     public ReviewEvaluationResponse update(ReviewEvaluationServiceRequest request) {
         Optional<ReviewEvaluation> reviewEvaluation = evaluationQueryRepository.searchByReviewAndMember(request.getReviewId(), request.getMemberId());
@@ -32,7 +40,15 @@ public class ReviewEvaluationService {
 
         Map<ReviewEvaluationType, Integer> evaluationCountsMap = evaluationQueryRepository.getCountByReviewIdGroupedByType(request.getReviewId());
 
+        MemberTier memberTier = memberTierQueryRepository.searchMemberTierByMemberId(request.getMemberId(), request.getGenreId());
+        if (request.getType().equals(BAD)) {
+            memberTier.decreaseExp();
+        } else {
+            memberTier.increaseExp(request.getType());
+        }
+
         return createReviewEvaluationResponse(request, isDone, evaluationCountsMap);
+
     }
 
     /**
